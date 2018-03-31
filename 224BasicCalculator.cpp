a36fb4ddef4bfe5e15c8979ffcff6565587d7b85
tianjinyuan <tianjinyuan@tianjinuandembp.attlocal.net>
2018-03-30 22:04:35 -0500

    change name

---
+++ 224BasicCalculator.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<char> operator_stack;
+        stack<int> num_stack;
+        int i = 0;
+
+        while(i < s.size()){
+            if(s[i] == ' '){
+                i++;
+                continue;
+            }
+
+            if(isdigit(s[i])){
+                int j = i;
+                while(j < s.size() && isdigit(s[j])) j++;
+                num_stack.push(stoi(s.substr(i, j-i)));
+                i = j;
+            }
+            else if(s[i] == '('){
+                operator_stack.push('(');
+                i++;
+            }
+            else if(s[i] == ')'){
+                while(operator_stack.top() != '('){
+                    char op = operator_stack.top();
+                    operator_stack.pop();
+                    calculate(num_stack, op);
+                }
+                operator_stack.pop();
+                i++;
+            }
+            else{
+                while(operator_stack.size() && operator_stack.top() != '('){
+                    char op = operator_stack.top();
+                    operator_stack.pop();
+                    calculate(num_stack, op);
+                }
+                operator_stack.push(s[i]);
+                i++;
+            }
+        }
+
+        while(operator_stack.size()){
+            char op = operator_stack.top();
+            operator_stack.pop();
+            calculate(num_stack, op);
+        }
+
+        return num_stack.top();
+    }
+
+    void calculate(stack<int>& num_stack, char op){
+        if(op == '+'){
+            int x = num_stack.top();
+            num_stack.pop();
+            int y = num_stack.top();
+            num_stack.pop();
+            num_stack.push(x+y);
+        }
+        else if(op == '-'){
+            int x = num_stack.top();
+            num_stack.pop();
+            int y = num_stack.top();
+            num_stack.pop();
+            num_stack.push(y-x);
+        }
+    }
+};
